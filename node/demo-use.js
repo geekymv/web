1aa85a693e3a38c24f7643e5c774c67a9fcc9f94
geekymv <ym2011678@foxmail.com>
2023-03-21 23:22:29 +0800

    CommonJS 规范

--- node/demo-use.js
+++ node/demo-use.js
@@ -5,3 +5,9 @@ console.log(demo)
 
 console.log(demo.print())
 console.log(demo.hello())
+
+/**
+ * CommonJS 规范
+ * module.exports、exports 以及 require 这些都是 CommonJS 模块化规范中的内容
+ * Node.js 是实现了 CommonJS 模块化规范，二者关系有点像 javascript 和 ECMAScript
+ */
