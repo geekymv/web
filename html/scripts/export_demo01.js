b45b3643e18a299af973bd912b6d50ec77ed8cf5
geekymv <ym2011678@foxmail.com>
2023-03-22 14:38:46 +0800

    ES6 export import

--- html/scripts/export_demo01.js
+++ html/scripts/export_demo01.js
@@ -1,6 +1,30 @@
-export let name = "export";
+// 方式1
+/*
+export var name = 'tom';
 
 export function print() {
     return "func print";
 }
+ */
 
+// 方式2
+/*
+var name = 'tom';
+function print() {
+    return 'func print';
+}
+export {
+    name,
+    print
+}
+*/
+
+// 方式3
+// 一个文件内只能有一个 export default
+const name = 'tom'
+export default {
+    name,
+    print: function () {
+        return "func print";
+    }
+}
