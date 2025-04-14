fad85257d15b4fb7cbed1c3798b3a96b0b8612ce
geekymv <ym2011678@foxmail.com>
2025-04-14 23:48:49 +0800

    命名导入

--- esm/demo02/index.js
+++ esm/demo02/index.js
@@ -1,4 +1,13 @@
 // 导入全部，通用
-import * as school from './school.js'
+// import * as school from './school.js'
 
-console.log(school)
+// console.log(school)
+
+// 命名导入
+import {
+    name,
+    address,
+    getTel
+} from './school.js'
+
+console.log(`school name: ${name}, address:${address}, tel: ${getTel()}`)
